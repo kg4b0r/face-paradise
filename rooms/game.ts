0ae225537415ffae5f8d9f66f1098570a414ce9c
dressi <dressi89@gmail.com>
2019-03-09 01:37:52 +0100

    send gameConfig added

--- rooms/game.ts
+++ rooms/game.ts
@@ -1,18 +1,51 @@
 const nanoid = require('nanoid');
-import {Room} from "colyseus";
+import {Client, Room} from "colyseus";
 import {Player} from "../src/player";
 import {EventType, Message} from "../src/message";
+import {StateType} from "../src/state";
 
 export class Game extends Room {
+    minPlayers = 0;
     maxClients = 5;
     players: Player[] = [];
 
+    sourceGameImageList = [
+        'base_64_image_1',
+        'base_64_image_2',
+        'base_64_image_3',
+        'base_64_image_4',
+        'base_64_image_5',
+        'base_64_image_6',
+        'base_64_image_7',
+        'base_64_image_8',
+        'base_64_image_9',
+        'base_64_image_11',
+        'base_64_image_12',
+        'base_64_image_13',
+        'base_64_image_14',
+        'base_64_image_15',
+        'base_64_image_16',
+        'base_64_image_17',
+        'base_64_image_18',
+        'base_64_image_19',
+        'base_64_image_21',
+        'base_64_image_22',
+        'base_64_image_23',
+        'base_64_image_24',
+        'base_64_image_25',
+        'base_64_image_26',
+        'base_64_image_27',
+        'base_64_image_28',
+        'base_64_image_29',
+    ];
+
     state = {
-        mainState: 'lobby',
         playerCount: 0,
-        gameImageList: {},
-        faceImageList: {},
-        voteRound: 0
+        mainState: StateType.Lobby,
+        gameImageList: [],
+        faceImageList: [],
+        voteRound: 0,
+        hostSessionId: ''
     };
 
     onInit(options) {
@@ -30,16 +63,33 @@ export class Game extends Room {
     onMessage(client, message: Message) {
         switch (message.event) {
             case EventType.Start:
+                console.log(this.players.length);
+                console.log(this.minPlayers);
+
+                if (this.players.length < this.minPlayers)
+                {
+                    this.send(client, new Message(EventType.InvalidStart, "Not enough player!!!"));
+                }
+                else
+                {
+                    this.state.mainState = StateType.Game;
+                    let gameImageKeys = Array.from(this.sourceGameImageList.keys());
+                    gameImageKeys = shuffle(gameImageKeys);
+                    this.clients.forEach(function (playerClient: Client) {
+                        this.send(playerClient, new Message(EventType.GameConfig, gameImageKeys.splice(0,5)));
+                    }, this);
+                }
                 break;
 
             case EventType.AvatarUpload:
                 const avatarId = nanoid(8);
                 this.state.faceImageList[avatarId] = message.data;
 
+                this.state.hostSessionId = client.sessionId;
+
                 this.players[client.sessionId] = new Player(avatarId);
 
                //TODO: this.clock.setTimeout(this.startVote, 10000, this.state);
-                this.state.mainState = 'hello';
                 break;
 
             case EventType.FaceImagesUpload:
@@ -71,4 +121,23 @@ export class Game extends Room {
     onDispose() {
     }
 
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
---
+++ src/state.ts
@@ -0,0 +1,6 @@
+export enum StateType {
+    Lobby = 'lobby',
+    Game = 'game',
+    Vote = 'vote',
+    Result = 'result'
+}
